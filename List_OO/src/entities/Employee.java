e918cc7ed037be61adf2d3aaef527f21d9db08e3
Bruno H. Homem <brunohhomem@gmail.com>
2023-11-04 11:51:34 -0300

    ex-lista

---
+++ List_OO/src/entities/Employee.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class Employee {
+
+	private Integer Id;
+	private String name;
+	private Double salary;
+
+	public Employee() {
+
+	}
+
+	public Employee(Integer id, String name, Double salary) {
+		Id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public void increaseSalary(double percentage) {
+		salary += salary * percentage / 100;
+	}
+
+	public String toString() {
+		return Id + ", " + name + ", " + String.format("%.2f", salary);
+	}
+}
