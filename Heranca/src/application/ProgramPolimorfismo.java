881a6a67806ae0711142a7ed5802145a2dd17a8b
Bruno H. Homem <brunohhomem@gmail.com>
2023-11-28 10:22:59 -0300

    fix

--- Heranca/src/application/ProgramPolimorfismo.java
+++ Heranca/src/application/ProgramPolimorfismo.java
@@ -9,14 +9,14 @@ public class ProgramPolimorfismo {
 		// polimorfismo - muitas formas
 		// mesma operação em variaveis do msm tipo, mas com comportamento diferentes
 		// (devido aos objetos que elas referenciam)
-		Account x = new Account(1020, "Ferdinand", 1000.0);
-		Account y = new SavingsAccount(1023, "Oswald", 1000.0, 0.01);
-
-		x.withdraw(50);
-		y.withdraw(50);
-
-		System.out.println(x.getBalance());
-		System.out.println(y.getBalance());
+//		Account x = new Account(1020, "Ferdinand", 1000.0);
+//		Account y = new SavingsAccount(1023, "Oswald", 1000.0, 0.01);
+//
+//		x.withdraw(50);
+//		y.withdraw(50);
+//
+//		System.out.println(x.getBalance());
+//		System.out.println(y.getBalance());
 
 	}
 }
--- Heranca/src/application/ProgramSuperClass.java
+++ Heranca/src/application/ProgramSuperClass.java
@@ -36,16 +36,16 @@ public class ProgramSuperClass {
 //			System.out.println("Update");
 //		}
 
-		Account acc1 = new Account(1001, "Alex", 1000.0);
-		acc1.withdraw(200);
-		System.out.println(acc1.getBalance());
-
-		Account acc2 = new SavingsAccount(1002, "Anna", 1000.0, 0.01);
-		acc2.withdraw(200);
-		System.out.println(acc2.getBalance());
-
-		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500);
-		acc3.withdraw(200);
-		System.out.println(acc3.getBalance());
+//		Account acc1 = new Account(1001, "Alex", 1000.0);
+//		acc1.withdraw(200);
+//		System.out.println(acc1.getBalance());
+//
+//		Account acc2 = new SavingsAccount(1002, "Anna", 1000.0, 0.01);
+//		acc2.withdraw(200);
+//		System.out.println(acc2.getBalance());
+//
+//		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500);
+//		acc3.withdraw(200);
+//		System.out.println(acc3.getBalance());
 	}
 }
