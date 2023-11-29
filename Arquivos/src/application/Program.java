e8388983a10a6a3629ab6c61f760b76eaf8bd36b
Bruno H. Homem <brunohhomem@gmail.com>
2023-11-29 15:55:22 -0300

    file information

--- Arquivos/src/application/Program.java
+++ Arquivos/src/application/Program.java
@@ -8,30 +8,15 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter a folder path: ");
+		System.out.println("Enter a file path: ");
 		String strPath = sc.nextLine();
-
+		
 		File path = new File(strPath);
-
-		File[] folders = path.listFiles(File::isDirectory); // traz as pastas do caminho especificado
-
-		System.out.println("Folders:");
-
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
-
-		File[] files = path.listFiles(File::isFile); // traz os arquivos do caminho especificado
+		
 		System.out.println();
-		System.out.println("Files: ");
-
-		for (File file : files) {
-			System.out.println(file);
-		}
-
-		System.out.println();
-		boolean success = new File(strPath + "\\subdir").mkdir(); // criando uma subpasta no diretorio
-		System.out.println("Directory created successfully: " + success);
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
 
 		sc.close();
 	}
